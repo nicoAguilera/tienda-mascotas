2063cff0565fe99f98deefa0cf7fd21b2b8e8d8d
Nicolas Aguilera <enaguilera@unsl.edu.ar>
2018-11-06 15:16:34 -0300

    Actualizando Modelos con los campos y relaciones correspondientes

--- app/Especie.php
+++ app/Especie.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Especie extends Model
 {
-    //
+    protected $guarded = ['id'];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['nombre'];
+	/**
+	 * Relationship
+	 */
+	public function mascotas()
+	{
+		return $this->hasMany('App\Mascota');
+	}
 }
